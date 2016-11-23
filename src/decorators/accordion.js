1a057378de42750a615475c9a6405effec9c95fd
Roman <kaboom.ua@gmail.com>
2016-11-23 21:27:58 +0200

    Update accordion.js

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 
 export default (Component) => class Accordion extends React.Component {
     state = {
+        //не привязывайся к названиям сущностей в декораторах, вся их суть в том, чтобы использовать с разными компонентами и данными. Сделай openItemId
         openArticleId: null
     }
     render() {
